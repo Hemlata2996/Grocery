f9da9ce381eaa6989f6ed34ce8700c56149cf78d
Hemlata2996 <75047312+Hemlata2996@users.noreply.github.com>
2020-11-26 09:56:36 +1000

    Update HomeScreen.js

--- grocery-app/src/Screens/HomeScreen.js
+++ grocery-app/src/Screens/HomeScreen.js
@@ -19,9 +19,7 @@ function HomeScreen(props) {
           </div>
           <span className="product-price">${product.price}</span>
           <span className="product-rating">{product.rating} stars</span>
-         
-        
-        </div>
+      </div>
       </li>
         )
     }  </ul>
